84ce3b99d84c6dc5d5e441c0a3b5ad90fa3d8a1d
Lara Sanders <larasanders@gmail.com>
2018-01-03 19:30:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-     puts params = {(name: params["name"], email: params["email"], password: params["password"])}
+     puts params
     # @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     # @user.save
     # User.create(name: params["name"], email: params["email"], password: params["password"])

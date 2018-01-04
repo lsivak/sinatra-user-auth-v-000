102063a87097d8c352c1837792cc82fa659b74e2
Lara Sanders <larasanders@gmail.com>
2018-01-04 00:15:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     erb :'/registrations/signup'
   end
 
-  post '/registrations' do
+  post '/registrations/signup' do
 
       @user = User.create(name: params["name"], email: params["email"], password: params["password"])
       @user.save
